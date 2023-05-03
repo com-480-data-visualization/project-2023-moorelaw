7e4417f3bc4631cf1bf6cf829b870edb46d986f1
Cyan Lin <xusine@outlook.com>
2023-05-03 22:08:54 +0200

    d3.js is ready

--- src/components/MooresLaw.vue
+++ src/components/MooresLaw.vue
@@ -4,37 +4,35 @@ import * as d3 from "d3";
 </script>
 
 <template>
-    <div class="section flex">
-        <h1>Moore's Law</h1>
-        <svg></svg>
-    </div>
+  <div class="section flex">
+    <h1>Moore's Law</h1>
+    <svg id="sub_svg"></svg>
+  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {};
-  },
-  mounted() {
-    const width = 800;
-    const height = 500;
-    const data = [
-    { date: "24-Apr-07", amount: 93.24 },
-    { date: "25-Apr-07", amount: 95.35 },
-    { date: "26-Apr-07", amount: 98.84 },
-    { date: "27-Apr-07", amount: 99.92 },
-    { date: "30-Apr-07", amount: 99.8 },
-    { date: "1-May-07", amount: 99.47 },
-    { date: "2-May-07", amount: 100.39 },
-    { date: "3-May-07", amount: 100.4 },
-    { date: "4-May-07", amount: 100.81 },
-    { date: "7-May-07", amount: 103.92 },
-    { date: "8-May-07", amount: 105.06 },
-    { date: "9-May-07", amount: 106.88 },
-    { date: "10-May-07", amount: 107.34 },
-  ];
 
-    const svg = d3.select("svg").attr("width", width).attr("height", height);
+export default {
+  mounted() {
+    const width = 300;
+    const height = 150;
+    const data = [
+      { date: "24-Apr-07", amount: 93.24 },
+      { date: "25-Apr-07", amount: 95.35 },
+      { date: "26-Apr-07", amount: 98.84 },
+      { date: "27-Apr-07", amount: 99.92 },
+      { date: "30-Apr-07", amount: 99.8 },
+      { date: "1-May-07", amount: 99.47 },
+      { date: "2-May-07", amount: 100.39 },
+      { date: "3-May-07", amount: 100.4 },
+      { date: "4-May-07", amount: 100.81 },
+      { date: "7-May-07", amount: 103.92 },
+      { date: "8-May-07", amount: 105.06 },
+      { date: "9-May-07", amount: 106.88 },
+      { date: "10-May-07", amount: 107.34 },
+    ];
+
+    const svg = d3.select("#sub_svg").attr("width", width).attr("height", height);
     const g = svg.append("g");
 
     //2. Parse the dates
@@ -82,7 +80,7 @@ export default {
       .attr("y", 6)
       .attr("dy", "0.71em")
       .attr("text-anchor", "end")
-      .text("Price ($)");    
+      .text("Price ($)");
 
     //6. Appending a path to the Chart
     g.append("path")
@@ -93,4 +91,5 @@ export default {
       .attr("d", line);
   },
 };
+
 </script>
