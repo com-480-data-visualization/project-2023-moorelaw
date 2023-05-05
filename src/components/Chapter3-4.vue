378619b0ea992cdaafa608126532bc0a5c75db6e
Cyan Lin <xusine@outlook.com>
2023-05-05 18:01:38 +0200

    sync

---
+++ src/components/Chapter3-4.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import Skylake from './SvgFigure/Skylake.vue';
+
+</script>
+
+<template>
+    <h2>Example: Intel Skylake (2015)</h2>
+    <div class="flex mt-5 h-auto">
+        <img src="/figure/skylake.webp" class="w-3/7 ml-20">
+        <Skylake class="rotate-90 w-4/10 h-2/5"></Skylake>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        mounted() {
+            console.log("xxx:", document.getElementById("path18245"));
+        }
+    }
+</script>
--- src/components/Chapter3.vue
+++ src/components/Chapter3.vue
@@ -2,6 +2,7 @@
     import Chapter31 from './Chapter3-1.vue';
     import Chapter32 from './Chapter3-2.vue';
     import Chapter33 from './Chapter3-3.vue';
+    import Chapter34 from './Chapter3-4.vue';
     import Skylake from './SvgFigure/Skylake.vue';
 </script>
 <template>
@@ -9,8 +10,6 @@
         <div class="slide"><Chapter31></Chapter31></div>
         <div class="slide"><Chapter32></Chapter32></div>
         <div class="slide"><Chapter33></Chapter33></div>
-        <div class="slide bg-dark-900 w-full">
-            <Skylake class="w-full h-79/80"></Skylake>
-        </div>
+        <div class="slide"><Chapter34></Chapter34></div>
     </div>
 </template>
