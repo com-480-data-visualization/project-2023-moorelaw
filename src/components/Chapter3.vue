57740aea046385ba31251a6b953b2d3171090380
Cyan Lin <xusine@outlook.com>
2023-05-05 16:26:21 +0200

    fix: skylake image is not embedded

--- src/components/Chapter3.vue
+++ src/components/Chapter3.vue
@@ -2,14 +2,15 @@
     import Chapter31 from './Chapter3-1.vue';
     import Chapter32 from './Chapter3-2.vue';
     import Chapter33 from './Chapter3-3.vue';
+    import Skylake from './SvgFigure/Skylake.vue';
 </script>
 <template>
     <div class="section flex text-center">
         <div class="slide"><Chapter31></Chapter31></div>
         <div class="slide"><Chapter32></Chapter32></div>
         <div class="slide"><Chapter33></Chapter33></div>
-        <div class="slide bg-dark-900">
-            <img src="/figure/skylake.svg" class="h-79/80">
+        <div class="slide bg-dark-900 w-full">
+            <Skylake class="w-full h-79/80"></Skylake>
         </div>
     </div>
 </template>
