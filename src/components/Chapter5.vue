379309fb35863bbdc55474eebddf633200af8831
Zhiyao Feng <zhiyaofeng22@gmail.com>
2023-06-03 20:57:32 +0200

    remove 5-2

--- src/components/Chapter5.vue
+++ src/components/Chapter5.vue
@@ -1,6 +1,5 @@
 <script setup>
 import Chapter51 from './Chapter5-1.vue';
-import Chapter52 from "./Chapter5-2.vue";
 import Chapter53 from "./Chapter5-3.vue";
 </script>
 
@@ -8,9 +7,6 @@ import Chapter53 from "./Chapter5-3.vue";
     <div class="section text-center">
         <div class="slide">
             <Chapter51></Chapter51>
-        </div>
-        <div class="slide">
-            <Chapter52></Chapter52>
         </div>
         <div class="slide">
             <Chapter53></Chapter53>
